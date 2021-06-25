33b2dfe177ae85deedb19e9102c9c0bd633bc632
Omar <57999049+ahmedmoutaalim@users.noreply.github.com>
2021-06-25 09:45:47 +0100

    modification

--- UnitTest_Mock/UnitTest_Mock/obj/Debug/netcoreapp3.1/UnitTest_Mock.AssemblyInfo.cs
+++ UnitTest_Mock/UnitTest_Mock/obj/Debug/netcoreapp3.1/UnitTest_Mock.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("8a4eff4a-5b71-435b-9ea5-28d92056d870")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTest_Mock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
